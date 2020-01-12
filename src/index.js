122d81badcace784843f71b42660fde0800a703e
Harshaldalvi7797 <[dalviharshal7797@gmail.com]>
2020-01-13 01:29:53 +0530

    commit : Apply Redux

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import {createStore,applyMiddleware} from "redux";
 import './index.css';
 import App from './App';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import reducers from "./redux/store/index";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-<BrowserRouter>
+    <Provider store={createStore(reducers,applyMiddleware(Thunk))}>
+        <BrowserRouter>
 <App />
 
 </BrowserRouter>
+
+    </Provider>
+
 
 , 
     
