608b55954e3f161ddbabcb0cb2b72f21d41b2773
Harshaldalvi7797 <[dalviharshal7797@gmail.com]>
2020-01-14 23:58:13 +0530

    Commit : Make authReducer .

---
+++ src/redux/reducer/auth.js
@@ -0,0 +1,9 @@
+export const AuthReducer = (state {} , action ) =>
+{
+    switch(action.type)
+    {
+        case : "FETCH_AUTHDATA" :
+            return action.payload
+            default : return state;
+    }
+}
